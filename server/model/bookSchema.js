98989a07edd803cad887db13ad164529cfefa4cd
Alexander Ruban <alexruban29@gmail.com>
2025-10-02 10:41:45 -0700

    Created the book schema.

--- server/model/bookSchema.js
+++ server/model/bookSchema.js
@@ -1,2 +1,30 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../db/db";
+
+const BookSchema = sequelize.define("pet", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isbn: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+});
+
+await BookSchema.sync({ force: true });
+
+export default BookSchema;

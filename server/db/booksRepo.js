ce51f58712a6e6237c3871fab07cb3134f99fdbc
f3liz <felixchen14@gmail.com>
2025-10-13 23:42:15 -0700

    made update repo for backend

--- server/db/booksRepo.js
+++ server/db/booksRepo.js
@@ -9,4 +9,11 @@ export const getAllBooks = async () => {
     return await BookSchema.findAll();
 }
 // U - Update
+export const updateBook = async (book) => {
+    return await BookSchema.update(book, {
+        where: {
+            id: book.id
+        }
+    })
+}
 // D - Delete

26357d7d93e2db15e9e25e88438d0eda417ebf03
Alexander Ruban <alexruban29@gmail.com>
2025-10-13 23:50:48 -0700

    Fixed config file for pm2 during VM deployment.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
   apps: [
     {
       name: "Our-Shelves-Backend",
-      cwd: "./Our-Shelves/server",
+      cwd: "./server",
       script: "node",
       args: "index.js",
       env: {
@@ -39,21 +39,29 @@ module.exports = {
       watch: false,
       max_restarts: 10,
       restart_delay: 5000,
-      out_file: "/var/log/ourshelves/out.log",
-      error_file: "/var/log/ourshelves/err.log",
+      out_file: "/var/log/ourshelves/backend-out.log",
+      error_file: "/var/log/ourshelves/backend-err.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss"
     },
     {
       name: "Our-Shelves-Frontend",
-      cwd: "./Our-Shelves/client",
+      cwd: "./client",
       script: "npm",
       args: "run start",
       env: {
         NODE_ENV: "production",
         PORT: 3001,
         HOST: "0.0.0.0"
-      }
+      },
+      autorestart: true,
+      watch: false,
+      max_restarts: 10,
+      restart_delay: 5000,
+      out_file: "/var/log/ourshelves/frontend-out.log",
+      error_file: "/var/log/ourshelves/frontend-err.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss"
     }
   ]
 };
+
 
